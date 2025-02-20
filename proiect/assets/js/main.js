ae9fa043db750591c677128457d341e7c5685cac
raaaul127 <raaul122704@gmail.com>
2025-02-20 15:11:55 +0200

    proiect

--- proiect/assets/js/main.js
+++ proiect/assets/js/main.js
@@ -94,6 +94,32 @@
   }
 
   /**
+   * Dark Mode
+   */
+
+  const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const currentTheme = localStorage.getItem('theme');
+
+if(currentTheme){
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
+
+function switchTheme(e) {
+    if(e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    } 
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+    }
+}
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+  /**
    * Back to top button
    */
   let backtotop = select('.back-to-top')
